abee957755c1d0ddd5d4b0548f4f97ddea5e8f6b
Ioannis Tsakpinis <iotsakp@gmail.com>
2017-02-20 16:06:51 +0200

    Android performance experiments

--- modules/core/src/main/java/org/lwjgl/system/ThreadLocalUtil.java
+++ modules/core/src/main/java/org/lwjgl/system/ThreadLocalUtil.java
@@ -9,6 +9,7 @@ import org.lwjgl.PointerBuffer;
 
 import java.lang.reflect.Field;
 import java.util.*;
+import java.util.function.*;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
@@ -126,7 +127,10 @@ public final class ThreadLocalUtil {
 
 	public static PointerBuffer getAddressesFromCapabilities(Object caps) {
 		List<Field> functions = Stream.of(caps.getClass().getFields())
-			.filter(f -> f.getType() == long.class)
+			.filter(new Predicate<Field>() {
+				@Override
+				public boolean test(Field f) {return f.getType() == long.class;}
+			})
 			.collect(Collectors.toList());
 
 		PointerBuffer addresses = BufferUtils.createPointerBuffer(functions.size());
